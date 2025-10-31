0113f9438ffb8dac642a58c87e3fcfcb20dd6495
keito-ux <keito.nakanishi2002@gmail.com>
2025-10-31 17:29:44 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
+// 👇 GitHub Pages用のベースパスを指定
 export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': process.env,
   },
-=======
-// 👇 GitHubリポジトリ名をここに書く！
-export default defineConfig({
-  plugins: [react()],
->>>>>>> 1e98cb38 (add Bolt project files)
   base: '/advent-calendar/',
 })
